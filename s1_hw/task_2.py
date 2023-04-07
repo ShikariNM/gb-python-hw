5f04111765ca864cec17ca992de0d2dc6acb850f
Nikita Mavrinsky <sharabarovnv@gmail.com>
2023-04-07 11:00:51 +0600

    done 2nd task

---
+++ s1_hw/task_2.py
@@ -0,0 +1,13 @@
+# Задача 2: Найдите сумму цифр трехзначного числа.
+
+# 123 -> 6 (1 + 2 + 3)
+# 100 -> 1 (1 + 0 + 0) |
+
+num = int(input())
+res = 0
+if num < 0: num *= -1
+while num > 0:
+    digit = num % 10
+    res += digit
+    num //= 10
+print(res)
